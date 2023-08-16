43e35bf7945bb4a8054e46c92f1c910980a302a1
GhostDaGhost <53021768+GhostDaGhost@users.noreply.github.com>
2023-08-16 10:58:30 -0700

    modify(adriana_bowtique): Work Commit

--- src/components/CarouselOfPictures.tsx
+++ src/components/CarouselOfPictures.tsx
@@ -5,6 +5,11 @@ import FeaturedPicture2 from '../assets/featured2.jpg';
 import FeaturedPicture3 from '../assets/featured3.jpg';
 import FeaturedPicture4 from '../assets/featured4.jpg';
 
+import FeaturedPicture5 from '../assets/featured5.jpg';
+import FeaturedPicture6 from '../assets/featured6.jpg';
+import FeaturedPicture7 from '../assets/featured7.jpg';
+import FeaturedPicture8 from '../assets/featured8.jpg';
+
 // CAROUSEL OF PICTURES
 const CarouselOfPictures = () => {
     return (
@@ -23,6 +28,22 @@ const CarouselOfPictures = () => {
 
             <div>
                 <img src={FeaturedPicture4} alt="featured picture #4" draggable="false" loading="lazy" />
+            </div>
+
+            <div>
+                <img src={FeaturedPicture5} alt="featured picture #5" draggable="false" loading="lazy" />
+            </div>
+
+            <div>
+                <img src={FeaturedPicture6} alt="featured picture #6" draggable="false" loading="lazy" />
+            </div>
+
+            <div>
+                <img src={FeaturedPicture7} alt="featured picture #7" draggable="false" loading="lazy" />
+            </div>
+
+            <div>
+                <img src={FeaturedPicture8} alt="featured picture #8" draggable="false" loading="lazy" />
             </div>
         </Carousel>
     )
